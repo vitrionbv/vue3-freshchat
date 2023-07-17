5bf20e7146592fc804fa5d888e0a8f88bfe2a0a3
diongrendelman <dion@vitrion.nl>
2023-07-17 11:58:27 +0200

    Changed vite builder

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,22 @@ import { resolve } from "node:path";
 // https://vitejs.dev/config/
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue()],
   build: {
     lib: {
+      // src/indext.ts is where we have exported the component(s)
       entry: resolve(__dirname, "src/index.ts"),
       name: "FreshChat",
-      fileName: "fresh-chat",
+      // the name of the output files when the build is run
+      fileName: "vue3-freshchat",
     },
     rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
       external: ["vue"],
       output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
         globals: {
           vue: "Vue",
         },
